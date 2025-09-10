a1fd3fe9899162c55e82dcc1668dafd5c80613da
Paulo Spachi <paulouem@gmail.com>
2025-09-10 18:58:50 +0000

    atualiza a página `index.js` para mostrar mensagem de construção

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-  return <h1>Oi gatinha, me fala como foi seu dia ;)</h1>;
+  return (
+    <>
+      <h1>Seja bem-vindo ao Revisão Saúde</h1>
+      <h2>Um portal para advogados</h2>
+      <p>
+        Em breve você terá um espaço para revisar planos de saúde empresariais.
+      </p>
+    </>
+  );
 }
 
 export default Home;
