fd8abf0c7cc60bc39d108c140c1d03d4101ad34f
Paulo Spachi <51764897+PauloSpachi85@users.noreply.github.com>
2025-09-23 19:25:52 +0000

    finaliza o endpoint `api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,29 @@
 import database from "infra/database.js"
 
 async function status(request, response) {
-  const result = await database.query('select 1 + 1 as sum;');
-  console.log(result.rows);
-  response.status(200).json({ chave: "são acima da média" });
+  const updatedAt = new Date().toISOString()
+
+  const dbVersionResult = await database.query("SHOW server_version;");
+  const databaseVersion = dbVersionResult.rows?.[0]?.server_version;
+
+  const dbMaxConnResult = await database.query("SHOW max_connections;");
+  const databaseMaxConnections = parseInt(dbMaxConnResult.rows[0].max_connections);
+
+  const databaseName = process.env.POSTGRES_DB;
+  const dbUsedConnResult = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName]
+  });
+  const databaseOpenedConnections = dbUsedConnResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database_version: databaseVersion,
+      database_max_connections: databaseMaxConnections,
+      database_opened_connections: databaseOpenedConnections
+    }
+  });
+  
 }
 export default status;
