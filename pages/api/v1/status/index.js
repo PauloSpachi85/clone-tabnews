960a5cd2e799e44875bf692d43056eb168055991
Paulo Spachi <51764897+PauloSpachi85@users.noreply.github.com>
2025-10-12 23:24:49 +0000

    adiciona o endpoint `/api/v1/migrations`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,18 +1,20 @@
-import database from "infra/database.js"
+import database from "infra/database.js";
 
 async function status(request, response) {
-  const updatedAt = new Date().toISOString()
+  const updatedAt = new Date().toISOString();
 
   const dbVersionResult = await database.query("SHOW server_version;");
   const databaseVersion = dbVersionResult.rows?.[0]?.server_version;
 
   const dbMaxConnResult = await database.query("SHOW max_connections;");
-  const databaseMaxConnections = parseInt(dbMaxConnResult.rows[0].max_connections);
+  const databaseMaxConnections = parseInt(
+    dbMaxConnResult.rows[0].max_connections,
+  );
 
   const databaseName = process.env.POSTGRES_DB;
   const dbUsedConnResult = await database.query({
     text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
-    values: [databaseName]
+    values: [databaseName],
   });
   const databaseOpenedConnections = dbUsedConnResult.rows[0].count;
 
@@ -21,9 +23,8 @@ async function status(request, response) {
     dependencies: {
       database_version: databaseVersion,
       database_max_connections: databaseMaxConnections,
-      database_opened_connections: databaseOpenedConnections
-    }
+      database_opened_connections: databaseOpenedConnections,
+    },
   });
-  
 }
 export default status;
